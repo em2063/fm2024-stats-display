31b3fd45fc9a421de5ed4e48c0c36538830b96b6
em2063 <em2063@hw.ac.uk>
2024-04-22 09:37:17 +0100

    read html using pd

--- stats.py
+++ stats.py
@@ -1,12 +1,9 @@
-from bs4 import BeautifulSoup #import Beautiful soup for html manipulation file
+import pandas as pd #import pd for html manipulation
 
 # Open file that holds imported team stats from football manager game
 with open("stats.html", "r") as stats_file:
     player_stats = stats_file.read()
 
-#configure bs
-soup = BeautifulSoup(player_stats, 'html.parser')
-
-stats_table = soup.find('table')
-table_header = stats_table.find_all("tr")[0]
-table_columns = table_header.find_all("th")
+#configure pd
+stats_data_list = pd.read_html(player_stats, header=0, encoding="utf-8")
+stats_data = stats_data_list[0]
