473532e6bb4a53f57fdb23f5b1a4e882e6c085ee
em2063 <em2063@hw.ac.uk>
2024-04-22 10:12:58 +0100

    adds algorithm for calculating goalkeeper ratings

--- stats.py
+++ stats.py
@@ -6,4 +6,22 @@ with open("stats.html", "r") as stats_file:
 
 #configure pd
 stats_data_list = pd.read_html(player_stats, header=0, encoding="utf-8")
-stats_data = stats_data_list[0]
+stats_data = stats_data_list[0] #turn into dataframe
+
+#calculate basic workrate factors to add into dataframe
+stats_data['Spd'] = (stats_data['Acc'] + stats_data['Pac']) / 2 #Calculate speed
+stats_data['Work'] = (stats_data['Wor'] + stats_data['Sta']) / 2 #Calculate Workrate
+stats_data['SetP'] = (stats_data['Jum'] + stats_data['Bra'])
+
+#calculate rating for goalkeeper position
+stats_data['gk_essential'] = (stats_data['Agi'] + stats_data['Ref']) * 5 #calculate most preferred stats rating
+
+stats_data['gk_core'] = (stats_data['Cmd'] + stats_data['Kic'] + stats_data['1v1']
+ + stats_data['Ant'] + stats_data['Cnt'] + stats_data['Pos']) * 3 #calculate required stats rating
+
+stats_data['gk_secondary'] = (stats_data['Aer'] + stats_data['Com'] + stats_data['Fir'] + stats_data['Han']
+ + stats_data['Pas'] + stats_data['TRO'] + stats_data['Thr'] + stats_data['Vis'] + stats_data['Dec'] + stats_data['Acc']
+ + stats_data['Cmp']) #calculate rating for useful stats
+
+#Calculate final average for goalkeeper stats
+stats_data['gk'] = (stats_data['gk_essential'] + stats_data['gk_core'] + stats_data['gk_secondary']) / 39
